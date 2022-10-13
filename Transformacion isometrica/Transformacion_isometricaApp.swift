46b616dc24f3b9294f0587a8bb692cbeb5df6220
victortejeda <losmejoresxxx28@gmail.com>
2022-10-13 00:34:34 -0400

    Initial Commit

---
+++ Transformacion isometrica/Transformacion_isometricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transformacion_isometricaApp.swift
+//  Transformacion isometrica
+//
+//  Created by Victor Tejeda on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Transformacion_isometricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
